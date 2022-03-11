ceeec1829c346eca7a0b4fd1a9e9c39d05485675
coder21-space <97867819+coder21-space@users.noreply.github.com>
2022-03-10 20:34:57 -0800

    message modal complete

--- Admin pannel/assets/ajax/contact-data.js
+++ Admin pannel/assets/ajax/contact-data.js
@@ -1,6 +1,7 @@
 $("#previous-tab").click(function (e) {
   // ajax calll
   output = "";
+  // output_error = "";
   $.ajax({
     url: "PHP/contact-data.php",
     type: "POST",
@@ -22,9 +23,29 @@ $("#previous-tab").click(function (e) {
 
         $("#contact").html(output);
       } else {
-        // show server error message
+        // output_error += `
+        //                                 <div class="card">
+        //             <div class="card-body p-4">
+        //                 <div class="text-center">
+        //                     <h1 class="text-error">404</h1>
+        //                     <h3 class="mt-3 mb-2">Page not Found</h3>
+        //                     <p class="text-muted mb-3">It's looking like you may have taken a wrong turn. Don't worry... it happens to
+        //                             the best of us. You might want to check your internet connection. Here's a little tip that might
+        //                             help you get back on track.</p>
+        //                     <a href="index.html" class="btn btn-danger waves-effect waves-light"><i class="fas fa-home me-1"></i> Back to Home</a>
+        //                 </div>
+        //             </div>
+        //             </div>`;
+        // $("#contact_error").html(output_error);
       }
     },
     error: function (error) {},
   });
 });
+
+// $(document).on("click", ".delete", function () {
+//   var id = $(this).attr("data-id");
+//   console.log(id);
+//   $("#contact_delete").val(id);
+//   $("#exampleModal").modal("show");
+// });
